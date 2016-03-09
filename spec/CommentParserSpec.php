f97f6d11a09c3d15a508c93f29f2bd863aba6db8
Greg Hermo <greg.hermo@gmail.com>
2016-03-09 23:04:14 +0800

    prime factor

--- spec/CommentParserSpec.php
+++ spec/CommentParserSpec.php
@@ -12,7 +12,35 @@ class CommentParserSpec extends ObjectBehavior
         $this->shouldHaveType('CommentParser');
     }
 
-    function it_parses_something() {
-    	$this->parse()->shouldReturn([]);
+    function it_returns_2_2_for_4() {
+    	$this->parse(4)->shouldReturn([2,2]);
+    }
+
+    function it_returns_2_2_2_for_8() {
+    	$this->parse(8)->shouldReturn([2,2,2]);
+    }
+
+    function it_returns_3_for_3() {
+    	$this->parse(3)->shouldReturn([3]);
+    }
+
+    function it_returns_3_3_for_9() {
+    	$this->parse(9)->shouldReturn([3,3]);
+    }
+
+    function it_returns_2_5_for_10() {
+        $this->parse(10)->shouldReturn([2,5]);
+    }
+
+    function it_returns_2_7_for_14() {
+        $this->parse(14)->shouldReturn([2,7]);
+    }
+
+    function it_computes_prime_for_144() {
+        $this->parse(144)->shouldReturn([2,2,2,2,3,3]);   
+    }
+
+    function it_computes_prime_for_360() {
+        $this->parse(360)->shouldReturn([2,2,2,3,3,5]);   
     }
 }
--- src/CommentParser.php
+++ src/CommentParser.php
@@ -2,8 +2,29 @@
 
 class CommentParser
 {
-    public function parse()
+    public function parse($number)
     {
-        return [];
+    	$prime = [];
+        while ($number > 1) {
+        	if ($number % 2 == 0) {
+    	    		$prime[] = 2;
+    	    		$number /= 2;
+    	    	}
+            if ($number % 3 == 0) {
+                $prime[] = 3;
+                $number /= 3;
+            }
+            if ($number % 5 == 0) {
+                $prime[] = 5;
+                $number /= 5;
+            }
+            if ($number % 7 == 0) {
+                $prime[] = 7;
+                $number /= 7;
+            }
+        }
+        sort($prime, SORT_NUMERIC);
+        var_dump($prime);
+        return $prime;
     }
 }
